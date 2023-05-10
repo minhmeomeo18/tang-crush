a3eaece29cae95d63c27671d09617d1b59667d90
minhmeomeo18 <88937823+minhmeomeo18@users.noreply.github.com>
2023-05-10 23:26:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em gái à :3',
+    btnNo: 'Không, em iu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà 🥰. Yêu Anh nhiều nhiều 😘😘',
-    messDesc: 'Đừng giận nữa anh nhá.',
+    messDesc: 'Yêu em mãi anh nhá <33333333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
